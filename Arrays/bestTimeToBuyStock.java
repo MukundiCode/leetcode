42f14e48dc2ab809d5727748728efa92423febaf
unknown <tinashechitamba@outlook.com>
2023-07-31 21:54:04 +0200

    best time to buy and sell

---
+++ Arrays/bestTimeToBuyStock.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int maxProfit(int[] prices) {
+        int buy = Integer.MAX_VALUE;
+        int profit = 0;
+
+        for (int i = 0; i < prices.length; i++){
+            buy = (prices[i] < buy) ? prices[i] : buy;
+            profit = (prices[i]-buy > profit) ? prices[i]-buy : profit;
+        }
+      
+        return profit;
+    }
+}
