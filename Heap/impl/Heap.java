ba13112754cb3439efbdfa12a38a688bd3c823e9
unknown <tinashechitamba@outlook.com>
2023-08-14 22:37:57 +0200

    heap

---
+++ Heap/impl/Heap.java
@@ -0,0 +1,29 @@
+public class Heap{
+
+	public static void Main(String... args){
+
+		int[] nums = {1,2,3,4,5,6,7,8,9};
+
+
+	}
+
+	public static Node createMinHeap(int[] nums, int index, Node head){
+		if (head == null) 
+
+
+	}
+
+}
+
+
+public class Node {
+	int val;
+
+	Node left;
+
+	Node right;
+
+	public Node(int val){
+		this.val = val;
+	}
+}
